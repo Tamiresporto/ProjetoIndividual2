e622302a237a0165bfcb24471ccf669747cf3baa
Tamires <112409145+Tamiresporto@users.noreply.github.com>
2022-10-03 10:15:58 -0300

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+/*Cifra de Cesar
+
+A  B  C  D  E  F  G  H  I  J  K  L  M
+0  1  2  3  4  5  6  7  8  9 10 11  12
+
+N  O  P  Q  R  S  T  U  V  W  X  Y  Z
+13 14 15 16 17 18 19 20 21 22 23 24 25
+
+*/
+
+5 + 7
+
+(codigoDaletra + desloc) % tamDoAlfabeto =>
+(codigoDaletra + 7) % 26
+
+
+
+((codigoDaletraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
+
+(70 - 65)
